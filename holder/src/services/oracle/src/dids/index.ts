a37821335f3db903cf4d36c7604047de9f70cc06
Adrian Ross <amr424@lehigh.edu>
2023-12-05 16:50:40 -0500

    remove local service

--- holder/src/services/oracle/src/dids/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  OracleDidRegistrar,
-  OracleDidCreateOptions,
-  OracleDidDeactivateOptions,
-  OracleDidUpdateOptions,
-} from "./OracleDidRegistrar";
-export { OracleDidResolver } from "./OracleDidResolver";
